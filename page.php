119583ea743fca1d9312072acc4faf733fbd953d
PatraSuchi <101924210+Suchibrata-Patra@users.noreply.github.com>
2025-01-03 21:33:29 +0530

    Update page.php

--- page.php
+++ page.php
@@ -22,11 +22,10 @@ get_header();
     </div>
 
     <script>
-       window.onload = function () {
+    window.onload = function () {
     const performanceData = window.performance.timing;
     const navigationStart = performanceData.navigationStart;
     const domContentLoadedTime = performanceData.domContentLoadedEventEnd - navigationStart;
-
     const domContentLoadedTimeInSec = (domContentLoadedTime / 1000).toFixed(3);
     document.getElementById('dom-content-loaded').textContent = domContentLoadedTimeInSec + ' seconds';
 };
