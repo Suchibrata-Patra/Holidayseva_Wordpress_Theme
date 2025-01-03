30c7f5ce85001bea6398d6f5db8856cf556211b5
PatraSuchi <101924210+Suchibrata-Patra@users.noreply.github.com>
2025-01-03 16:08:45 +0530

    header Introduced

---
+++ header.php
@@ -0,0 +1,8 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="<?php  echo get_template_directory_uri(); ?>/style.css">
+    <title>Holidayseva</title>
+</head>
--- index.php
+++ index.php
@@ -3,16 +3,7 @@
     echo '<br>';
     bloginfo('template_directory');
 ?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="<?php  echo get_template_directory_uri(); ?>/style.css">
-    <title>Document</title>
-</head>
 <body>
-
     <div class="hero_section_home_page">
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae tempora nemo ratione! Sequi officiis aut nobis sit nihil minima quam in. Accusantium consectetur qui earum doloribus itaque reprehenderit harum commodi.
     </div>
