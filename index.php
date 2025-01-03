4cd51a10ff79ef22f70d00584c1eb0a126272eab
PatraSuchi <101924210+Suchibrata-Patra@users.noreply.github.com>
2025-01-03 16:09:13 +0530

    Update index.php

--- index.php
+++ index.php
@@ -2,12 +2,11 @@
     echo get_template_directory_uri();
     echo '<br>';
     bloginfo('template_directory');
+    get_headers();
 ?>
 <body>
     <div class="hero_section_home_page">
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae tempora nemo ratione! Sequi officiis aut nobis sit nihil minima quam in. Accusantium consectetur qui earum doloribus itaque reprehenderit harum commodi.
     </div>
-    
-    
 </body>
 </html>
