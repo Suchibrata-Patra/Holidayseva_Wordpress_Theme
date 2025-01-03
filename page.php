d93efc6195ebd880627b86006f052406f74c765e
PatraSuchi <101924210+Suchibrata-Patra@users.noreply.github.com>
2025-01-03 17:08:30 +0530

    Update page.php

--- page.php
+++ page.php
@@ -6,7 +6,7 @@ get_header();
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title><?php echo the_title(); ?></title>
+    <title><?php the_title(); ?></title>
 </head>
 <body>
     
