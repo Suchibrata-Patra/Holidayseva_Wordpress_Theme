87b89ed28a77e3ff60cbf3c3a46bd68c7f51c2af
PatraSuchi <101924210+Suchibrata-Patra@users.noreply.github.com>
2025-01-03 17:10:56 +0530

    Update page.php

--- page.php
+++ page.php
@@ -1,14 +1,25 @@
 <?php 
 get_header();
 ?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title><?php the_title(); ?></title>
+    <title><?php wp_title(); ?></title>
 </head>
 <body>
-    <?php echo the_title(); ?>
+    <?php 
+    if ( have_posts() ) :
+        while ( have_posts() ) : the_post();
+            echo the_title(); // Or just echo get_the_title()
+        endwhile;
+    endif;
+    ?>
 </body>
 </html>
+
+<?php 
+get_footer();
+?>
