129e2bf3fd0960869e5f3318fe15b9030791084c
PatraSuchi <101924210+Suchibrata-Patra@users.noreply.github.com>
2025-01-05 04:15:16 +0530

    Changed

--- functions.php
+++ functions.php
@@ -12,6 +12,34 @@
             'id'=>'sidebar'
         )
     );
-    
+    function create_book_post_type() {
+    $args = array(
+        'labels' => array(
+            'name' => 'Books',
+            'singular_name' => 'Book',
+            'add_new' => 'Add New Book',
+            'add_new_item' => 'Add New Book',
+            'edit_item' => 'Edit Book',
+            'new_item' => 'New Book',
+            'view_item' => 'View Book',
+            'search_items' => 'Search Books',
+            'not_found' => 'No Books found',
+            'not_found_in_trash' => 'No Books found in Trash',
+            'all_items' => 'All Books',
+            'insert_into_item' => 'Insert into book',
+            'uploaded_to_this_item' => 'Uploaded to this book',
+        ),
+        'public' => true,
+        'hierarchical' => false, // Books are not hierarchical themselves
+        'supports' => array('title', 'editor', 'author', 'thumbnail', 'excerpt', 'comments'),
+        'menu_icon' => 'dashicons-book',
+        'show_in_rest' => true, // Enable Gutenberg block editor
+        'has_archive' => true,
+        'rewrite' => array('slug' => 'books'),
+    );
+    register_post_type('book', $args);
+}
+add_action('init', 'create_book_post_type');
+
     
 ?>
