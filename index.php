71a18527842c5ce6a07644a99d96e9229afde3b8
PatraSuchi <101924210+Suchibrata-Patra@users.noreply.github.com>
2025-01-05 00:35:01 +0530

    Changed

--- index.php
+++ index.php
@@ -4,45 +4,9 @@
     bloginfo('template_directory');
     get_header();
 ?>
-<?php
-/**
- * The template for displaying all pages
- *
- * This is the template that displays all pages by default.
- * Please note that this is the WordPress construct of pages and that
- * other 'pages' on your WordPress site will use a different template.
- *
- * @package WordPress
- * @subpackage Twenty_Fourteen
- * @since Twenty Fourteen 1.0
- */
-
-get_header(); ?>
-
-<div id="main-content" class="main-content">
-	<div id="primary" class="content-area">
-		<div id="content" class="site-content" role="main">
-
-			<?php
-			// Start the Loop.
-			while ( have_posts() ) :
-				the_post();
-
-				// Include the page content template.
-				get_template_part( 'content', 'page' );
-
-				// If comments are open or we have at least one comment, load up the comment template.
-				if ( comments_open() || get_comments_number() ) {
-					comments_template();
-				}
-				endwhile;
-			?>
-
-		</div><!-- #content -->
-	</div><!-- #primary -->
-	<?php get_sidebar( 'content' ); ?>
-</div><!-- #main-content -->
-
-<?php
-get_sidebar();
-get_footer();
+<body>
+    <div class="hero_section_home_page">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae tempora nemo ratione! Sequi officiis aut nobis sit nihil minima quam in. Accusantium consectetur qui earum doloribus itaque reprehenderit harum commodi.
+    </div>
+</body>
+</html>
