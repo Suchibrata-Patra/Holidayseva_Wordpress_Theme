7954afb1efe433247ff9827d87baccb0b53da8ca
PatraSuchi <101924210+Suchibrata-Patra@users.noreply.github.com>
2025-01-05 04:09:48 +0530

    Update functions.php

--- functions.php
+++ functions.php
@@ -12,5 +12,39 @@
             'id'=>'sidebar'
         )
     );
+    function create_custom_post_type() {
+        $args = array(
+            'labels' => array(
+                'name' => 'Books',
+                'singular_name' => 'Book',
+                'add_new' => 'Add New Book',
+                'add_new_item' => 'Add New Book',
+                'edit_item' => 'Edit Book',
+                'new_item' => 'New Book',
+                'view_item' => 'View Book',
+                'search_items' => 'Search Books',
+                'not_found' => 'No Books found',
+                'not_found_in_trash' => 'No Books found in Trash',
+                'all_items' => 'All Books',
+                'archives' => 'Books Archives',
+                'insert_into_item' => 'Insert into book',
+                'uploaded_to_this_item' => 'Uploaded to this book',
+            ),
+            'public' => true,  // Make the post type public
+            'has_archive' => true, // Enable archive page
+            'show_in_rest' => true, // Enable block editor
+            'rewrite' => array('slug' => 'books'), // URL structure (optional)
+            'supports' => array('title', 'editor', 'author', 'thumbnail', 'excerpt', 'comments'), // Features supported
+            'menu_icon' => 'dashicons-book',  // Icon for the menu (optional)
+            'show_ui' => true,  // Show in WordPress admin
+            'show_in_menu' => true,
+            'show_in_nav_menus' => true,
+            'show_in_admin_bar' => true,
+        );
+    
+        register_post_type('book', $args);
+    }
+    add_action('init', 'create_custom_post_type');
+    
     
 ?>
