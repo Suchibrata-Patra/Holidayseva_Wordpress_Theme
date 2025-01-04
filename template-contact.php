aa2d2d460b1b0593a3be149ead0c6983f24e32a2
PatraSuchi <101924210+Suchibrata-Patra@users.noreply.github.com>
2025-01-05 00:16:11 +0530

    Update template-contact.php

--- template-contact.php
+++ template-contact.php
@@ -1,31 +1,19 @@
 <?php
-// Template Name: Contact
+//Template Name: Contact
 ?>
-<?php get_header(); ?>
-
+<?php get_header();?>
 <?php
-  // Check if we have content for the current page
-  if(have_posts()) {
-    while(have_posts()) {
-      the_post();
-      $imagepath = wp_get_attachment_image_src(get_post_thumbnail_id(), 'large');
+	while(have_posts()){
+		the_post();
+				$imagepath = wp_get_attachment_image_src(get_post_thumbnail_id(),'large');
 ?>
-
-      <div>
-        <?php if ($imagepath): ?>
-          <img src="<?php echo esc_url($imagepath[0]); ?>" alt="<?php the_title(); ?>">
-        <?php endif; ?>
-        <h2><?php the_title(); ?></h2>
-        <p><?php echo get_the_date(); ?></p>
-        <p><?php the_excerpt(); ?></p>
-        <?php the_content(); ?>
-      </div>
-
-<?php 
-    }
-  } else {
-    echo '<p>No content found.</p>';
-  }
+	<div>
+		<img src="<?php echo $imagepath[0] ?>">
+		<h2><?php the_title(); ?></h2>
+		<?php echo get_the_date(); ?>
+		<?php echo get_the_excerpt(); ?>
+	</div>
+	
+<?php
+} 
 ?>
-
-<?php get_footer(); ?>
