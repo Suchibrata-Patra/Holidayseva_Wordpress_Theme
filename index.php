954518fe469c79d1400aed97fc9a1a0980651335
PatraSuchi <101924210+Suchibrata-Patra@users.noreply.github.com>
2025-01-05 04:17:14 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,30 +1,12 @@
 <?php 
-    // Output the URL of the theme directory (alternative method)
-    echo get_template_directory_uri();
+    // echo get_template_directory_uri();
     echo '<br>';
-    
-    // Alternative method (commented out)
     // bloginfo('template_directory');
-    
-    // Get the header
-    get_header(); 
+    get_header();
+    get_post();
 ?>
-    <div>
-        <h1>This page Comes from index.php</h1>
-        
-        <!-- Output the content of the current post or page -->
-        <?php
-        if (have_posts()) : 
-            while (have_posts()) : the_post(); 
-                the_content(); // This outputs the content of the post/page
-            endwhile;
-        else : 
-            echo '<p>No content found</p>';
-        endif;
-        ?>
-    </div>
-    
-<?php 
-    // Get the footer
-    get_footer();
-?>
+<body>
+   This page Comes from index.php
+   <?php the_content(); ?>
+</body>
+</html>
