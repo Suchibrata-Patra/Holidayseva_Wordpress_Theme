140d1b29493014fd69edca7ae9762c8badf03f3f
PatraSuchi <101924210+Suchibrata-Patra@users.noreply.github.com>
2025-01-05 00:25:46 +0530

    Update template-posts.php

--- template-posts.php
+++ template-posts.php
@@ -1,19 +1,30 @@
 <?php
 //Template Name: Blog Post
 ?>
-<?php get_header();?>
+<?php get_header(); ?>
+
 <?php
-	while(have_posts()){
-		the_post();
-?>
-	<div>
-		<img src="<?php echo $imagepath[0] ?>">
-		<h2><?php the_title(); ?></h2>
-		<?php echo get_the_date(); ?>
-		<?php echo get_the_excerpt(); ?>
-	</div>
-	
-<?php
-} 
+  while (have_posts()) {
+    the_post();
+    // Get the post's featured image URL
+    $imagepath = wp_get_attachment_image_src(get_post_thumbnail_id(), 'large');
+    ?>
+    <div class="post">
+      <!-- Check if the post has a featured image -->
+      <?php if ($imagepath): ?>
+        <img src="<?php echo esc_url($imagepath[0]); ?>" alt="<?php the_title(); ?>">
+      <?php endif; ?>
+
+      <h2><?php the_title(); ?></h2>
+
+      <!-- Display the post's publish date -->
+      <p class="post-date"><?php echo get_the_date(); ?></p>
+
+      <!-- Display the post's excerpt -->
+      <p class="post-excerpt"><?php echo get_the_excerpt(); ?></p>
+    </div>
+  <?php
+  }
 ?>
 
+<?php get_footer(); ?>
