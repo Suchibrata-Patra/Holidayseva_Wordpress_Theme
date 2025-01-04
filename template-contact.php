3a647a9fb491416de7e37f57f2b9981f27605c6c
PatraSuchi <101924210+Suchibrata-Patra@users.noreply.github.com>
2025-01-04 21:25:28 +0530

    Update template-contact.php

--- template-contact.php
+++ template-contact.php
@@ -1,19 +1,29 @@
 <?php
-//Template Name: Contact
+// Template Name: Contact
+get_header();
 ?>
-<?php get_header();?>
-<?php
-	while(have_posts()){
-		the_post();
-				$imagepath = wp_get_attachment_image_src(get_post_thumbnail_id(),'large');
-?>
-	<div>
-		<img src="<?php echo $imagepath?>">
-		<h2><?php the_title() ?></h2>
-		<?php echo get_the_date(); ?>
-		<?php echo get_the_excerpt(); ?>
-	</div>
-	
-<?php
-} 
-?>
+
+<?php if ( have_posts() ) : ?>
+    <?php while ( have_posts() ) : the_post(); ?>
+
+        <!-- Featured Image -->
+        <?php if ( has_post_thumbnail() ) : ?>
+            <?php $imagepath = wp_get_attachment_image_src( get_post_thumbnail_id(), 'large' ); ?>
+            <img src="<?php echo $imagepath[0]; ?>" alt="<?php the_title(); ?>" />
+        <?php endif; ?>
+
+        <!-- Post Title -->
+        <h2><?php the_title(); ?></h2>
+
+        <!-- Post Date -->
+        <p><?php echo get_the_date(); ?></p>
+
+        <!-- Post Excerpt -->
+        <p><?php echo get_the_excerpt(); ?></p>
+
+    <?php endwhile; ?>
+<?php else : ?>
+    <p>No posts found.</p>
+<?php endif; ?>
+
+<?php get_footer(); ?>
