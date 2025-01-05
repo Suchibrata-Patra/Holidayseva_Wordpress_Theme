fed775ee2386badaae5b50718af026ddc2ef868d
PatraSuchi <101924210+Suchibrata-Patra@users.noreply.github.com>
2025-01-05 14:03:54 +0530

    Update single.php

--- single.php
+++ single.php
@@ -1,1 +1,12 @@
-lorem
+<?php 
+    // echo get_template_directory_uri();
+    echo '<br>';
+    // bloginfo('template_directory');
+    get_header();
+    get_post();
+?>
+<body>
+   This page Comes from index.php
+   <?php the_content(); ?>
+</body>
+</html>
