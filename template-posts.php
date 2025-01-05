98063255ad4659349c438bdef3c2a213d12a2f46
PatraSuchi <101924210+Suchibrata-Patra@users.noreply.github.com>
2025-01-05 19:42:48 +0530

    Update template-posts.php

--- template-posts.php
+++ template-posts.php
@@ -4,6 +4,14 @@
 <?php get_header(); ?>
 
 <?php
+  // Custom query to fetch multiple posts
+  $args = array(
+    'posts_per_page' => -1,  // Show all posts
+  );
+  $query = new WP_Query($args);
+
+  // The loop to display posts
+  if ($query->have_posts()) :
     while ($query->have_posts()) :
       $query->the_post();
       // Get the post's featured image URL
@@ -24,6 +32,7 @@
         <p class="post-excerpt"><?php echo get_the_excerpt(); ?></p>
       </div>
     <?php endwhile; ?>
+  <?php endif; ?>
   
   <!-- Reset post data after custom query -->
   <?php wp_reset_postdata(); ?>
