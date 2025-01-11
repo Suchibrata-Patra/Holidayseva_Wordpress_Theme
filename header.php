010946d4e90b24b6acfcd4c999d6e5ced1093f2b
PatraSuchi <101924210+Suchibrata-Patra@users.noreply.github.com>
2025-01-11 19:42:46 +0530

    Update header.php

--- header.php
+++ header.php
@@ -112,7 +112,7 @@
     .new-sidebar {
         position: fixed;
         top: 0;
-        left: -300px;
+        left: -3000px;
         width: 300px;
         height: 100%;
         background-color: #ffffff;
