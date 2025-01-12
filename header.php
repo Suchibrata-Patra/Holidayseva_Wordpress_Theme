01b0e20c3f74c3f97f8221320d256a78bd632c74
PatraSuchi <101924210+Suchibrata-Patra@users.noreply.github.com>
2025-01-12 20:18:25 +0530

    Favicon Changed

--- header.php
+++ header.php
@@ -4,7 +4,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title><?php wp_title(); ?></title>    
+    <title><?php wp_title(); ?></title>  
+    <link rel="icon" type="image/x-icon" href="<?php echo get_template_directory_uri();?>/Assets/Images/favicon.svg">  
 <?php
     $logo_image = get_header_image();
 ?>
