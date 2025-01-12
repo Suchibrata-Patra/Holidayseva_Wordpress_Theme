85a66bd8fb374a684b064b75254557b843fbcf70
PatraSuchi <101924210+Suchibrata-Patra@users.noreply.github.com>
2025-01-12 19:30:16 +0530

    Update header.php

--- header.php
+++ header.php
@@ -76,6 +76,7 @@
 ?>
 </aside>
 <div class="new-overlay"></div>
+<center>
     <div id="form-input">
         <div id="search-icon" style="color: #000000;">
             <span class="material-icons">search</span>
@@ -127,19 +128,19 @@
             }
         </style>
         <script>
-            document.addEventListener("DOMContentLoaded", function () {
-                const whatsappLink = document.getElementById("whatsapp-link");
-                // Fetch the URL of the current page
-                const currentPageURL = window.location.href;
-                // Generate the WhatsApp link with the default text
-                const defaultText = `Hello,
-I came across your website and explored this page : ${currentPageURL}.
-I'm interested in learning more about the tours you offer, Could You kindly share details about the itenary,pricing and any other relevant information ?`;
-                const whatsappURL = `https://wa.me/8145302135/?text=${encodeURIComponent(defaultText)}`;
-                // Update the anchor tag's href attribute
-                whatsappLink.href = whatsappURL;
-            });
-        </script>
+                document.addEventListener("DOMContentLoaded", function () {
+                    const whatsappLink = document.getElementById("whatsapp-link");
+                    // Fetch the URL of the current page
+                    const currentPageURL = window.location.href;
+                    // Generate the WhatsApp link with the default text
+                    const defaultText = `Hello,
+  I came across your website and explored this page : ${currentPageURL}.
+  I'm interested in learning more about the tours you offer, Could You kindly share details about the itenary,pricing and any other relevant information ?`;
+                    const whatsappURL = `https://wa.me/8145302135/?text=${encodeURIComponent(defaultText)}`;
+                    // Update the anchor tag's href attribute
+                    whatsappLink.href = whatsappURL;
+                });
+            </script>
         <script>
             // Ensure popup is hidden on page load
             document.addEventListener("DOMContentLoaded", function () {
