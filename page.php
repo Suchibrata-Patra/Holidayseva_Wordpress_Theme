3fcfaeb10256b5a3d9da73502a98f1a360bdc609
PatraSuchi <101924210+Suchibrata-Patra@users.noreply.github.com>
2025-01-13 20:29:24 +0530

    Update page.php

--- page.php
+++ page.php
@@ -9,7 +9,7 @@
     <h1>Images will load after the page is fully loaded</h1>
     <div class="image-container">
         <div class="placeholder delayed-image" data-src="https://s0.2mdn.net/simgad/6020499889114613682"></div>
-        <div class="placeholder delayed-image" data-src="https://s0.2mdn.net/simgad/6020499889114613682"></div>
+        <div class="placeholder delayed-image" data-src="https://s0.2mdn.net/simgasdsd/6020499889114613682"></div>
         <div class="placeholder delayed-image" data-src="https://s0.2mdn.net/simgad/6020499889114613682"></div>
     </div>
 
@@ -17,7 +17,7 @@
         /* Placeholder styling */
         .placeholder {
             width: 300px; /* Set a fixed width */
-            height: 200px; /* Set a fixed height */
+            height: auto; /* Set a fixed height */
             background: linear-gradient(90deg, #e0e0e0 25%, #f5f5f5 50%, #e0e0e0 75%);
             background-size: 200% 100%;
             animation: shimmer 1.5s infinite;
