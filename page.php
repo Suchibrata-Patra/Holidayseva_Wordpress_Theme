c1da61d6477ee453b756f4e898f05de44c7e2b8c
PatraSuchi <101924210+Suchibrata-Patra@users.noreply.github.com>
2025-01-13 20:39:51 +0530

    Update page.php

--- page.php
+++ page.php
@@ -12,7 +12,7 @@
 
     <h1>Images will load after the page is fully loaded</h1>
     <div class="image-container">
-        <img data-src="https://s0.2mdn.net/simgad/60204998891146136ss82" alt="Skeleton Loader" class="delayed-image" style="background-color:grey;">
+        <img data-src="https://s0.2mdn.net/simgad/60204998891146136sss82" alt="Skeleton Loader" class="delayed-image" style="background-color:rgb(248, 248, 248);border-radius: 10px;">
         <img data-src="https://s0.2mdn.net/simgad/6020499889114613682" alt="Skeleton Loader" class="delayed-image">
         <img data-src="https://s0.2mdn.net/simgad/6020499889114613682" alt="Skeleton Loader" class="delayed-image">
     </div>
