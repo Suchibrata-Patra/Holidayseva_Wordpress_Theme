e10e96816f21dc3effc9c34923d92dceedf741c5
PatraSuchi <101924210+Suchibrata-Patra@users.noreply.github.com>
2025-01-14 20:47:04 +0530

    Update functions.php

--- functions.php
+++ functions.php
@@ -15,27 +15,27 @@
     function create_book_post_type() {
     $args = array(
         'labels' => array(
-            'name' => 'Books',
+            'name' => 'Tour',
             'singular_name' => 'Book',
             'add_new' => 'Add New Book',
             'add_new_item' => 'Add New Book',
             'edit_item' => 'Edit Book',
             'new_item' => 'New Book',
             'view_item' => 'View Book',
-            'search_items' => 'Search Books',
-            'not_found' => 'No Books found',
-            'not_found_in_trash' => 'No Books found in Trash',
-            'all_items' => 'All Books',
+            'search_items' => 'Search Tour',
+            'not_found' => 'No Tour found',
+            'not_found_in_trash' => 'No Tour found in Trash',
+            'all_items' => 'All Tour',
             'insert_into_item' => 'Insert into book',
             'uploaded_to_this_item' => 'Uploaded to this book',
         ),
         'public' => true,
-        'hierarchical' => false, // Books are not hierarchical themselves
+        'hierarchical' => false, // Tour are not hierarchical themselves
         'supports' => array('title', 'editor', 'author', 'thumbnail', 'excerpt', 'comments'),
         'menu_icon' => 'dashicons-book',
         'show_in_rest' => true, // Enable Gutenberg block editor
         'has_archive' => true,
-        'rewrite' => array('slug' => 'books'),
+        'rewrite' => array('slug' => 'Tour'),
     );
     register_post_type('book', $args);
 }
