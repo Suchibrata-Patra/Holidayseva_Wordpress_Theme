f59de1bd36c04a7f47e55856282f41aa298ed29f
PatraSuchi <101924210+Suchibrata-Patra@users.noreply.github.com>
2025-01-14 20:49:17 +0530

    Update functions.php

--- functions.php
+++ functions.php
@@ -12,34 +12,34 @@
             'id'=>'sidebar'
         )
     );
-    function create_Tour_post_type() {
+    function create_book_post_type() {
     $args = array(
         'labels' => array(
             'name' => 'Tour',
-            'singular_name' => 'Tour',
-            'add_new' => 'Add New Tour',
-            'add_new_item' => 'Add New Tour',
-            'edit_item' => 'Edit Tour',
-            'new_item' => 'New Tour',
-            'view_item' => 'View Tour',
+            'singular_name' => 'Book',
+            'add_new' => 'Add New Book',
+            'add_new_item' => 'Add New Book',
+            'edit_item' => 'Edit Book',
+            'new_item' => 'New Book',
+            'view_item' => 'View Book',
             'search_items' => 'Search Tour',
             'not_found' => 'No Tour found',
             'not_found_in_trash' => 'No Tour found in Trash',
             'all_items' => 'All Tour',
-            'insert_into_item' => 'Insert into Tour',
-            'uploaded_to_this_item' => 'Uploaded to this Tour',
+            'insert_into_item' => 'Insert into book',
+            'uploaded_to_this_item' => 'Uploaded to this book',
         ),
         'public' => true,
         'hierarchical' => false, // Tour are not hierarchical themselves
         'supports' => array('title', 'editor', 'author', 'thumbnail', 'excerpt', 'comments'),
-        'menu_icon' => 'dashicons-Tour',
+        'menu_icon' => 'dashicons-book',
         'show_in_rest' => true, // Enable Gutenberg block editor
         'has_archive' => true,
         'rewrite' => array('slug' => 'Tour'),
     );
-    register_post_type('Tour', $args);
+    register_post_type('book', $args);
 }
-add_action('init', 'create_Tour_post_type');
+add_action('init', 'create_book_post_type');
 
     
 ?>
