1fa8ffd81fe66173b69ab466b47f159f3af3be6b
PatraSuchi <101924210+Suchibrata-Patra@users.noreply.github.com>
2025-01-14 20:46:50 +0530

    Update functions.php

--- functions.php
+++ functions.php
@@ -13,33 +13,33 @@
         )
     );
     function create_book_post_type() {
-        $args = array(
-            'labels' => array(
-                'name' => 'Tours',  // Changed from 'Books' to 'Tours'
-                'singular_name' => 'Tour',  // Changed from 'Book' to 'Tour'
-                'add_new' => 'Add New Tour',  // Changed
-                'add_new_item' => 'Add New Tour',  // Changed
-                'edit_item' => 'Edit Tour',  // Changed
-                'new_item' => 'New Tour',  // Changed
-                'view_item' => 'View Tour',  // Changed
-                'search_items' => 'Search Tours',  // Changed
-                'not_found' => 'No Tours found',  // Changed
-                'not_found_in_trash' => 'No Tours found in Trash',  // Changed
-                'all_items' => 'All Tours',  // Changed
-                'insert_into_item' => 'Insert into tour',  // Changed
-                'uploaded_to_this_item' => 'Uploaded to this tour',  // Changed
-            ),
-            'public' => true,
-            'hierarchical' => false, // Tours are not hierarchical
-            'supports' => array('title', 'editor', 'author', 'thumbnail', 'excerpt', 'comments'),
-            'menu_icon' => 'dashicons-book', // Optional: You might want to change the icon to one that represents tours
-            'show_in_rest' => true, // Enable Gutenberg block editor
-            'has_archive' => true,
-            'rewrite' => array('slug' => 'tours'),  // Changed from 'books' to 'tours'
-        );
-        register_post_type('tour', $args);  // Changed from 'book' to 'tour'
-    }
-    add_action('init', 'create_book_post_type');
-    
+    $args = array(
+        'labels' => array(
+            'name' => 'Books',
+            'singular_name' => 'Book',
+            'add_new' => 'Add New Book',
+            'add_new_item' => 'Add New Book',
+            'edit_item' => 'Edit Book',
+            'new_item' => 'New Book',
+            'view_item' => 'View Book',
+            'search_items' => 'Search Books',
+            'not_found' => 'No Books found',
+            'not_found_in_trash' => 'No Books found in Trash',
+            'all_items' => 'All Books',
+            'insert_into_item' => 'Insert into book',
+            'uploaded_to_this_item' => 'Uploaded to this book',
+        ),
+        'public' => true,
+        'hierarchical' => false, // Books are not hierarchical themselves
+        'supports' => array('title', 'editor', 'author', 'thumbnail', 'excerpt', 'comments'),
+        'menu_icon' => 'dashicons-book',
+        'show_in_rest' => true, // Enable Gutenberg block editor
+        'has_archive' => true,
+        'rewrite' => array('slug' => 'books'),
+    );
+    register_post_type('book', $args);
+}
+add_action('init', 'create_book_post_type');
+
     
 ?>
