fc1f85e166ed4c4847a4ca9e06c2a5bcc3c920dc
PatraSuchi <101924210+Suchibrata-Patra@users.noreply.github.com>
2025-01-14 20:45:42 +0530

    Update functions.php

--- functions.php
+++ functions.php
@@ -12,32 +12,32 @@
             'id'=>'sidebar'
         )
     );
-    function create_book_post_type() {
+    function create_Tour_post_type() {
     $args = array(
         'labels' => array(
-            'name' => 'Books',
-            'singular_name' => 'Book',
-            'add_new' => 'Add New Book',
-            'add_new_item' => 'Add New Book',
-            'edit_item' => 'Edit Book',
-            'new_item' => 'New Book',
-            'view_item' => 'View Book',
-            'search_items' => 'Search Books',
-            'not_found' => 'No Books found',
-            'not_found_in_trash' => 'No Books found in Trash',
-            'all_items' => 'All Books',
-            'insert_into_item' => 'Insert into book',
-            'uploaded_to_this_item' => 'Uploaded to this book',
+            'name' => 'Tours',
+            'singular_name' => 'Tour',
+            'add_new' => 'Add New Tour',
+            'add_new_item' => 'Add New Tour',
+            'edit_item' => 'Edit Tour',
+            'new_item' => 'New Tour',
+            'view_item' => 'View Tour',
+            'search_items' => 'Search Tours',
+            'not_found' => 'No Tours found',
+            'not_found_in_trash' => 'No Tours found in Trash',
+            'all_items' => 'All Tours',
+            'insert_into_item' => 'Insert into Tour',
+            'uploaded_to_this_item' => 'Uploaded to this Tour',
         ),
         'public' => true,
-        'hierarchical' => false, // Books are not hierarchical themselves
+        'hierarchical' => false, // Tours are not hierarchical themselves
         'supports' => array('title', 'editor', 'author', 'thumbnail', 'excerpt', 'comments'),
-        'menu_icon' => 'dashicons-book',
+        'menu_icon' => 'dashicons-Tour',
         'show_in_rest' => true, // Enable Gutenberg block editor
         'has_archive' => true,
-        'rewrite' => array('slug' => 'books'),
+        'rewrite' => array('slug' => 'Tours'),
     );
-    register_post_type('book', $args);
+    register_post_type('Tour', $args);
 }
-add_action('init', 'create_book_post_type');    
+add_action('init', 'create_Tour_post_type');    
 ?>
