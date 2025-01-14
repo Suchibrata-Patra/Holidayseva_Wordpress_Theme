3420d122895b786ab515172829c1bb7a0fa47d02
PatraSuchi <101924210+Suchibrata-Patra@users.noreply.github.com>
2025-01-14 23:05:37 +0530

    Update functions.php

--- functions.php
+++ functions.php
@@ -21,30 +21,6 @@ function create_custom_table() {
     dbDelta($sql);
 }
 
-// Register theme activation hook to create the table when the theme is activated
-function theme_activation() {
-    create_custom_table();  // Create the custom table on theme activation
-    // Other theme activation actions, if needed
-}
-add_action('after_switch_theme', 'theme_activation');
-
-// Register navigation menu
-register_nav_menus(
-    array('primary_menu' => 'Top Menu')
-);
-
-// Add theme support for post thumbnails and custom header
-add_theme_support('post-thumbnails');
-add_theme_support('custom-header');
-
-// Register sidebar location
-register_sidebar(
-    array(
-        'name' => 'Sidebar Location',
-        'id' => 'sidebar',
-    )
-);
-
 // Function to create a custom post type for Books
 function create_book_post_type() {
     $args = array(
@@ -73,37 +49,126 @@ function create_book_post_type() {
     );
     register_post_type('book', $args);
 }
-
-// Register the custom post type on 'init' action
 add_action('init', 'create_book_post_type');
-// Register custom taxonomy for "Destinations"
-function create_destination_taxonomy() {
-    $labels = array(
-        'name'              => 'Destinations',
-        'singular_name'     => 'Destination',
-        'search_items'      => 'Search Destinations',
-        'all_items'         => 'All Destinations',
-        'parent_item'       => 'Parent Destination',
-        'parent_item_colon' => 'Parent Destination:',
-        'edit_item'         => 'Edit Destination',
-        'update_item'       => 'Update Destination',
-        'add_new_item'      => 'Add New Destination',
-        'new_item_name'     => 'New Destination Name',
-        'menu_name'         => 'Destinations',
-    );
 
-    $args = array(
-        'hierarchical'      => true, // Makes it behave like categories
-        'show_ui'            => true,
-        'show_admin_column'  => true,
-        'query_var'          => true,
-        'rewrite'            => array('slug' => 'destination'),
+// Add custom fields for Book details
+function add_book_meta_boxes() {
+    add_meta_box(
+        'book_details_meta_box', // Meta box ID
+        'Book Details', // Title of the meta box
+        'display_book_meta_box', // Callback function to display the meta box
+        'book', // Post type where it will be displayed
+        'normal', // Position of the meta box
+        'high' // Priority of the meta box
     );
-
-    // Register the taxonomy for the "tour" custom post type
-    register_taxonomy('destination', array('tour'), $args);
 }
 
-add_action('init', 'create_destination_taxonomy');
+add_action('add_meta_boxes', 'add_book_meta_boxes');
 
+// Callback function to display custom fields in the meta box
+function display_book_meta_box($post) {
+    // Retrieve existing custom fields values
+    $book_name = get_post_meta($post->ID, '_book_name', true);
+    $book_isbn = get_post_meta($post->ID, '_book_isbn', true);
+    $book_details = get_post_meta($post->ID, '_book_details', true);
+    $book_author = get_post_meta($post->ID, '_book_author', true);
+    $book_publisher = get_post_meta($post->ID, '_book_publisher', true);
+    $book_publish_date = get_post_meta($post->ID, '_book_publish_date', true);
+    $book_language = get_post_meta($post->ID, '_book_language', true);
+    $book_genre = get_post_meta($post->ID, '_book_genre', true);
+    $book_pages = get_post_meta($post->ID, '_book_pages', true);
+    $book_cover_type = get_post_meta($post->ID, '_book_cover_type', true);
+    $book_price = get_post_meta($post->ID, '_book_price', true);
+    $book_stock = get_post_meta($post->ID, '_book_stock', true);
+    $book_edition = get_post_meta($post->ID, '_book_edition', true);
+
+    // Display custom fields in the meta box
+    ?>
+    <label for="book_name">Book Name:</label>
+    <input type="text" name="book_name" value="<?php echo esc_attr($book_name); ?>" class="widefat" />
+    
+    <label for="book_isbn">ISBN:</label>
+    <input type="text" name="book_isbn" value="<?php echo esc_attr($book_isbn); ?>" class="widefat" />
+    
+    <label for="book_details">Details:</label>
+    <textarea name="book_details" class="widefat"><?php echo esc_textarea($book_details); ?></textarea>
+    
+    <label for="book_author">Author:</label>
+    <input type="text" name="book_author" value="<?php echo esc_attr($book_author); ?>" class="widefat" />
+    
+    <label for="book_publisher">Publisher:</label>
+    <input type="text" name="book_publisher" value="<?php echo esc_attr($book_publisher); ?>" class="widefat" />
+    
+    <label for="book_publish_date">Publish Date:</label>
+    <input type="date" name="book_publish_date" value="<?php echo esc_attr($book_publish_date); ?>" class="widefat" />
+    
+    <label for="book_language">Language:</label>
+    <input type="text" name="book_language" value="<?php echo esc_attr($book_language); ?>" class="widefat" />
+    
+    <label for="book_genre">Genre:</label>
+    <input type="text" name="book_genre" value="<?php echo esc_attr($book_genre); ?>" class="widefat" />
+    
+    <label for="book_pages">Pages:</label>
+    <input type="number" name="book_pages" value="<?php echo esc_attr($book_pages); ?>" class="widefat" />
+    
+    <label for="book_cover_type">Cover Type:</label>
+    <input type="text" name="book_cover_type" value="<?php echo esc_attr($book_cover_type); ?>" class="widefat" />
+    
+    <label for="book_price">Price:</label>
+    <input type="number" name="book_price" value="<?php echo esc_attr($book_price); ?>" class="widefat" />
+    
+    <label for="book_stock">Stock:</label>
+    <input type="number" name="book_stock" value="<?php echo esc_attr($book_stock); ?>" class="widefat" />
+    
+    <label for="book_edition">Edition:</label>
+    <input type="text" name="book_edition" value="<?php echo esc_attr($book_edition); ?>" class="widefat" />
+    <?php
+}
+
+// Save custom fields values when the post is saved
+function save_book_meta($post_id) {
+    if (defined('DOING_AUTOSAVE') && DOING_AUTOSAVE) return $post_id;
+
+    if (isset($_POST['book_name'])) {
+        update_post_meta($post_id, '_book_name', sanitize_text_field($_POST['book_name']));
+    }
+    if (isset($_POST['book_isbn'])) {
+        update_post_meta($post_id, '_book_isbn', sanitize_text_field($_POST['book_isbn']));
+    }
+    if (isset($_POST['book_details'])) {
+        update_post_meta($post_id, '_book_details', sanitize_textarea_field($_POST['book_details']));
+    }
+    if (isset($_POST['book_author'])) {
+        update_post_meta($post_id, '_book_author', sanitize_text_field($_POST['book_author']));
+    }
+    if (isset($_POST['book_publisher'])) {
+        update_post_meta($post_id, '_book_publisher', sanitize_text_field($_POST['book_publisher']));
+    }
+    if (isset($_POST['book_publish_date'])) {
+        update_post_meta($post_id, '_book_publish_date', sanitize_text_field($_POST['book_publish_date']));
+    }
+    if (isset($_POST['book_language'])) {
+        update_post_meta($post_id, '_book_language', sanitize_text_field($_POST['book_language']));
+    }
+    if (isset($_POST['book_genre'])) {
+        update_post_meta($post_id, '_book_genre', sanitize_text_field($_POST['book_genre']));
+    }
+    if (isset($_POST['book_pages'])) {
+        update_post_meta($post_id, '_book_pages', intval($_POST['book_pages']));
+    }
+    if (isset($_POST['book_cover_type'])) {
+        update_post_meta($post_id, '_book_cover_type', sanitize_text_field($_POST['book_cover_type']));
+    }
+    if (isset($_POST['book_price'])) {
+        update_post_meta($post_id, '_book_price', floatval($_POST['book_price']));
+    }
+    if (isset($_POST['book_stock'])) {
+        update_post_meta($post_id, '_book_stock', intval($_POST['book_stock']));
+    }
+    if (isset($_POST['book_edition'])) {
+        update_post_meta($post_id, '_book_edition', sanitize_text_field($_POST['book_edition']));
+    }
+}
+
+add_action('save_post', 'save_book_meta');
 ?>
