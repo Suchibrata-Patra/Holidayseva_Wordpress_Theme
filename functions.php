66e6ceda0a63e4191a8caefc9478a011cfb34142
PatraSuchi <101924210+Suchibrata-Patra@users.noreply.github.com>
2025-01-15 21:19:34 +0530

    Changed

--- functions.php
+++ functions.php
@@ -45,7 +45,7 @@ function create_book_post_type() {
         'menu_icon' => 'dashicons-palmtree',
         'show_in_rest' => true, // Enable Gutenberg block editor
         'has_archive' => true,
-        'rewrite' => array('slug' => 'books'),
+        'rewrite' => array('slug' => 'tours'),
         'menu_position' => 2, // Add this line
     );
     register_post_type('book', $args);
