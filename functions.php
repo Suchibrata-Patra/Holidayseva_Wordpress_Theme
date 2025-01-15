1425769f4f74d2d55515c9c58b143ecf52826dce
PatraSuchi <101924210+Suchibrata-Patra@users.noreply.github.com>
2025-01-16 01:06:16 +0530

    Update functions.php

--- functions.php
+++ functions.php
@@ -50,6 +50,7 @@ function create_tour_post_type() {
         'menu_position' => 2,
     );
     register_post_type('tour', $args);
+    
 }
 add_action('init', 'create_tour_post_type');
 
@@ -160,11 +161,6 @@ function save_tour_meta($post_id) {
     if (isset($_POST['tour_availability'])) {
         update_post_meta($post_id, '_tour_availability', sanitize_text_field($_POST['tour_availability']));
     }
-
-    // RankMath's focus keyword
-    if (isset($_POST['_rank_math_focus_keyword'])) {
-        update_post_meta($post_id, '_rank_math_focus_keyword', sanitize_text_field($_POST['_rank_math_focus_keyword']));
-    }
 }
 
 add_action('save_post', 'save_tour_meta');
@@ -172,9 +168,110 @@ add_action('save_post', 'save_tour_meta');
 // Optionally, you can add the function to create a custom table (call create_custom_table when needed)
 add_action('after_switch_theme', 'create_custom_table');
 
-// Register the custom table creation hook
-function create_custom_table_on_activation() {
-    create_custom_table();
+// Create the options page under the "Settings" menu
+// Create the options page under the "Tours" menu
+function add_trip_options_page() {
+    add_submenu_page(
+        'edit.php?post_type=tour', // Parent menu slug for "Tours"
+        'Add Trip', // Page title
+        'Add Trip', // Menu title
+        'manage_options', // Capability
+        'add_trip', // Menu slug
+        'display_add_trip_page' // Function to display the options page content
+    );
 }
-register_activation_hook(__FILE__, 'create_custom_table_on_activation');
+add_action('admin_menu', 'add_trip_options_page');
 
+
+// Function to display the options page
+function display_add_trip_page() {
+    ?>
+    <div class="wrap">
+        <h1>Add a New Trip</h1>
+        <form method="post" action="">
+            <?php
+            // Check if the form has been submitted
+            if (isset($_POST['add_trip_submit'])) {
+                // Get the data from the form and sanitize it
+                $tour_name = sanitize_text_field($_POST['tour_name']);
+                $tour_details = sanitize_textarea_field($_POST['tour_details']);
+                $tour_location = sanitize_text_field($_POST['tour_location']);
+                $tour_duration = sanitize_text_field($_POST['tour_duration']);
+                $tour_price = floatval($_POST['tour_price']);
+                $tour_availability = sanitize_text_field($_POST['tour_availability']);
+                $tour_cover_images = sanitize_text_field($_POST['tour_cover_images']);
+
+                // Create a new post of type 'tour'
+                $tour_post = array(
+                    'post_title' => $tour_name,
+                    'post_content' => $tour_details,
+                    'post_status' => 'publish',
+                    'post_type' => 'tour',
+                    'meta_input' => array(
+                        '_tour_name' => $tour_name,
+                        '_tour_details' => $tour_details,
+                        '_tour_location' => $tour_location,
+                        '_tour_duration' => $tour_duration,
+                        '_tour_price' => $tour_price,
+                        '_tour_availability' => $tour_availability,
+                        '_tour_cover_images' => explode(',', $tour_cover_images),
+                    ),
+                );
+
+                // Insert the post into the database
+                $post_id = wp_insert_post($tour_post);
+
+                if ($post_id) {
+                    echo '<div class="updated"><p>Tour added successfully!</p></div>';
+                } else {
+                    echo '<div class="error"><p>Failed to add tour. Please try again.</p></div>';
+                }
+            }
+            ?>
+
+            <table class="form-table">
+                <tr valign="top">
+                    <th scope="row"><label for="tour_name">Tour Name:</label></th>
+                    <td><input type="text" name="tour_name" id="tour_name" class="regular-text" required /></td>
+                </tr>
+
+                <tr valign="top">
+                    <th scope="row"><label for="tour_details">Details:</label></th>
+                    <td><textarea name="tour_details" id="tour_details" rows="5" class="large-text" required></textarea></td>
+                </tr>
+
+                <tr valign="top">
+                    <th scope="row"><label for="tour_location">Location:</label></th>
+                    <td><input type="text" name="tour_location" id="tour_location" class="regular-text" required /></td>
+                </tr>
+
+                <tr valign="top">
+                    <th scope="row"><label for="tour_duration">Duration:</label></th>
+                    <td><input type="text" name="tour_duration" id="tour_duration" class="regular-text" required /></td>
+                </tr>
+
+                <tr valign="top">
+                    <th scope="row"><label for="tour_price">Price:</label></th>
+                    <td><input type="number" name="tour_price" id="tour_price" class="regular-text" required /></td>
+                </tr>
+
+                <tr valign="top">
+                    <th scope="row"><label for="tour_availability">Availability:</label></th>
+                    <td><input type="text" name="tour_availability" id="tour_availability" class="regular-text" required /></td>
+                </tr>
+
+                <tr valign="top">
+                    <th scope="row"><label for="tour_cover_images">Cover Images (comma-separated URLs):</label></th>
+                    <td><input type="text" name="tour_cover_images" id="tour_cover_images" class="regular-text" required />
+                    </td>
+                </tr>
+            </table>
+
+            <p class="submit">
+                <input type="submit" name="add_trip_submit" id="add_trip_submit" class="button-primary" value="Add Trip" />
+            </p>
+        </form>
+    </div>
+    <?php
+}
+?>
