c6d47cb18b17112ca6227d6479580257f8b08648
PatraSuchi <101924210+Suchibrata-Patra@users.noreply.github.com>
2025-01-16 17:50:57 +0530

    Update functions.php

--- functions.php
+++ functions.php
@@ -16,48 +16,6 @@ add_filter('rank_math/snippet/rich_snippet_data', function ($data, $post) {
     return $data;
 }, 10, 2);
 
-// Register custom post types
-function register_custom_post_types() {
-    $post_types = [
-        'custom_post' => [
-            'name' => 'Custom Posts',
-            'singular_name' => 'Custom Post',
-            'slug' => 'custom-posts',
-            'icon' => 'dashicons-admin-post',
-        ],
-        'tour' => [
-            'name' => 'Tours',
-            'singular_name' => 'Tour',
-            'slug' => 'tours',
-            'icon' => 'dashicons-palmtree',
-        ],
-    ];
-
-    foreach ($post_types as $slug => $type) {
-        register_post_type($slug, [
-            'labels' => [
-                'name' => $type['name'],
-                'singular_name' => $type['singular_name'],
-                'add_new' => "Add New {$type['singular_name']}",
-                'add_new_item' => "Add New {$type['singular_name']}",
-                'edit_item' => "Edit {$type['singular_name']}",
-                'new_item' => "New {$type['singular_name']}",
-                'view_item' => "View {$type['singular_name']}",
-                'search_items' => "Search {$type['name']}",
-                'not_found' => "No {$type['name']} found",
-                'not_found_in_trash' => "No {$type['name']} found in Trash",
-                'all_items' => "All {$type['name']}",
-            ],
-            'public' => true,
-            'supports' => ['title', 'editor', 'thumbnail', 'excerpt', 'comments'],
-            'menu_icon' => $type['icon'],
-            'show_in_rest' => true,
-            'has_archive' => true,
-            'rewrite' => ['slug' => $type['slug']],
-        ]);
-    }
-}
-add_action('init', 'register_custom_post_types');
 
 // Enqueue scripts for media uploader
 function enqueue_tour_scripts($hook) {
