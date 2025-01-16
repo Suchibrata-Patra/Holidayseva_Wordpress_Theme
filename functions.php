63ed49756a717afb26c632df5ee539db755bc25c
PatraSuchi <101924210+Suchibrata-Patra@users.noreply.github.com>
2025-01-16 11:15:08 +0530

    Update functions.php

--- functions.php
+++ functions.php
@@ -108,60 +108,72 @@ function display_tour_meta_box($post) {
     ?>
     <div class="container-fluid">
     <div class="row">
-    <!-- Sidebar -->
-    <div class="col-md-3">
-        <div class="list-group">
-            <a href="#tour_form" class="list-group-item list-group-item-action active" data-toggle="collapse" aria-expanded="true" aria-controls="tour_form">
-                Tour Details
-            </a>
-            <a href="#seo_form" class="list-group-item list-group-item-action" data-toggle="collapse" aria-expanded="false" aria-controls="seo_form">
-                SEO Settings
-            </a>
+        <!-- Sidebar -->
+        <div class="col-md-3">
+            <div class="list-group">
+                <a href="#tour_form" class="list-group-item list-group-item-action active" data-toggle="collapse" aria-expanded="true">
+                    Tour Details
+                </a>
+                <a href="#seo_form" class="list-group-item list-group-item-action" data-toggle="collapse" aria-expanded="false">
+                    SEO Settings
+                </a>
+            </div>
+        </div>
+
+        <!-- Main Content -->
+        <div class="col-md-9">
+            <div id="tour_form" class="collapse show">
+                <h3>Tour Details</h3>
+                <form method="post" action="">
+                    <div class="form-group">
+                        <label for="tour_name">Tour Name:</label>
+                        <input type="text" name="tour_name" value="<?php echo esc_attr($tour_name); ?>" class="form-control" />
+                    </div>
+
+                    <div class="form-group">
+                        <label for="tour_details">Details:</label>
+                        <textarea name="tour_details" class="form-control"><?php echo esc_textarea($tour_details); ?></textarea>
+                    </div>
+
+                    <div class="form-group">
+                        <label for="tour_location">Location:</label>
+                        <input type="text" name="tour_location" value="<?php echo esc_attr($tour_location); ?>" class="form-control" />
+                    </div>
+
+                    <div class="form-group">
+                        <label for="tour_duration">Duration:</label>
+                        <input type="text" name="tour_duration" value="<?php echo esc_attr($tour_duration); ?>" class="form-control" />
+                    </div>
+
+                    <div class="form-group">
+                        <label for="tour_price">Price:</label>
+                        <input type="number" name="tour_price" value="<?php echo esc_attr($tour_price); ?>" class="form-control" />
+                    </div>
+
+                    <div class="form-group">
+                        <label for="tour_availability">Availability:</label>
+                        <input type="text" name="tour_availability" value="<?php echo esc_attr($tour_availability); ?>" class="form-control" />
+                    </div>
+
+                    <div class="form-group">
+                        <label for="tour_cover_images">Cover Images:</label>
+                        <input type="text" name="tour_cover_images" id="tour_cover_images" value="<?php echo esc_attr(implode(',', (array)$tour_cover_images)); ?>" class="form-control" />
+                        <button type="button" id="tour_cover_images_button" class="btn btn-primary mt-2">Select Images</button>
+                    </div>
+                </form>
+            </div>
+
+            <div id="seo_form" class="collapse">
+                <h3>SEO Settings</h3>
+                <div class="form-group">
+                        <label for="tour_name">Tour Name:</label>
+                        <input type="text" name="tour_name" value="<?php echo esc_attr($tour_name); ?>" class="form-control" />
+                        <label for="tour_name">Tour Name:</label>
+                        <input type="text" name="tour_name" value="<?php echo esc_attr($tour_name); ?>" class="form-control" />
+                    </div>
+            </div>
         </div>
     </div>
-
-    <!-- Main Content -->
-    <div class="col-md-9">
-        <div id="tour_form" class="collapse show">
-            <h3>Tour Details</h3>
-            <form method="post" action="">
-                <!-- Tour Details Form -->
-                <div class="form-group">
-                    <label for="tour_name">Tour Name:</label>
-                    <input type="text" name="tour_name" value="<?php echo esc_attr($tour_name); ?>" class="form-control" />
-                </div>
-                <!-- Add other fields for the Tour form -->
-            </form>
-        </div>
-
-        <div id="seo_form" class="collapse">
-            <h3>SEO Settings</h3>
-            <form method="post" action="">
-                <!-- SEO Form -->
-                <div class="form-group">
-                    <label for="seo_title">SEO Title:</label>
-                    <input type="text" name="seo_title" value="" class="form-control" />
-                </div>
-                <div class="form-group">
-                    <label for="seo_description">SEO Description:</label>
-                    <textarea name="seo_description" class="form-control"></textarea>
-                </div>
-                <div class="form-group">
-                    <label for="focus_keyword">Focus Keyword:</label>
-                    <input type="text" name="focus_keyword" value="" class="form-control" />
-                </div>
-            </form>
-        </div>
-    </div>
-</div>
-
-<!-- Include Bootstrap CSS -->
-<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-
-<!-- Include jQuery and Bootstrap JS -->
-<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/js/bootstrap.bundle.min.js"></script>
-
 </div>
 
 <script type="text/javascript">
