1e0dfc81d43a139c277f9b99f18f1e61e3111847
PatraSuchi <101924210+Suchibrata-Patra@users.noreply.github.com>
2025-01-16 13:15:36 +0530

    Update functions.php

--- functions.php
+++ functions.php
@@ -1,7 +1,5 @@
 <?php
-// Add theme support for post thumbnails
 add_theme_support('post-thumbnails');
-
 add_filter( 'rank_math/snippet/rich_snippet_data', function( $data, $post ) {
     if ( 'tour' === $post->post_type ) {
         // Custom schema data for 'tour' post type
@@ -30,28 +28,6 @@ function register_custom_post_type() {
 }
 add_action('init', 'register_custom_post_type');
 
-
-
-// Function to create the custom table for bookings
-function create_custom_table() {
-    global $wpdb;
-    
-    $table_name = $wpdb->prefix . 'custom_bookings'; // Name of your custom table
-    $charset_collate = $wpdb->get_charset_collate();
-
-    // SQL query to create the table
-    $sql = "CREATE TABLE $table_name (
-        id mediumint(9) NOT NULL AUTO_INCREMENT,
-        customer_name varchar(255) NOT NULL,
-        tour_id mediumint(9) NOT NULL,
-        booking_date datetime DEFAULT CURRENT_TIMESTAMP NOT NULL,
-        payment_status varchar(20) NOT NULL,
-        PRIMARY KEY  (id)
-    ) $charset_collate;";
-
-    require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
-    dbDelta($sql);
-}
 
 // Function to create a custom post type for Tours
 function create_tour_post_type() {
@@ -94,7 +70,6 @@ function add_tour_meta_boxes() {
         'high' 
     );
 }
-
 add_action('add_meta_boxes', 'add_tour_meta_boxes');
 
 // Callback function to display custom fields in the meta box
@@ -351,100 +326,4 @@ function add_trip_options_page() {
 add_action('admin_menu', 'add_trip_options_page');
 
 // Function to display the options page
-function display_add_trip_page() {
-    ?>
-    <div class="wrap">
-        <h1>Add a New Trip</h1>
-        <form method="post" action="">
-            <?php
-            // Check if the form has been submitted
-            if (isset($_POST['add_trip_submit'])) {
-                // Get the data from the form and sanitize it
-                $tour_name = sanitize_text_field($_POST['tour_name']);
-                $tour_details = sanitize_textarea_field($_POST['tour_details']);
-                $tour_location = sanitize_text_field($_POST['tour_location']);
-                $tour_duration = sanitize_text_field($_POST['tour_duration']);
-                $tour_price = floatval($_POST['tour_price']);
-                $tour_availability = sanitize_text_field($_POST['tour_availability']);
-                $tour_cover_images = sanitize_text_field($_POST['tour_cover_images']);
-                $focus_keyword = sanitize_text_field($_POST['rank_math_focus_keyword']);
-
-                // Create a new post of type 'tour'
-                $tour_post = array(
-                    'post_title' => $tour_name,
-                    'post_content' => $tour_details,
-                    'post_status' => 'publish',
-                    'post_type' => 'tour',
-                    'meta_input' => array(
-                        '_tour_name' => $tour_name,
-                        '_tour_details' => $tour_details,
-                        '_tour_location' => $tour_location,
-                        '_tour_duration' => $tour_duration,
-                        '_tour_price' => $tour_price,
-                        '_tour_availability' => $tour_availability,
-                        '_tour_cover_images' => explode(',', $tour_cover_images),
-                        '_rank_math_focus_keyword' => $focus_keyword,
-                    ),
-                );
-
-                // Insert the post into the database
-                $post_id = wp_insert_post($tour_post);
-
-                if ($post_id) {
-                    echo '<div class="updated"><p>Tour added successfully!</p></div>';
-                } else {
-                    echo '<div class="error"><p>Failed to add tour. Please try again.</p></div>';
-                }
-            }
-            ?>
-
-            <table class="form-table">
-                <tr valign="top">
-                    <th scope="row"><label for="tour_name">Tour Name:</label></th>
-                    <td><input type="text" name="tour_name" id="tour_name" class="regular-text" required /></td>
-                </tr>
-
-                <tr valign="top">
-                    <th scope="row"><label for="tour_details">Details:</label></th>
-                    <td><textarea name="tour_details" id="tour_details" rows="5" class="large-text" required></textarea></td>
-                </tr>
-
-                <tr valign="top">
-                    <th scope="row"><label for="tour_location">Location:</label></th>
-                    <td><input type="text" name="tour_location" id="tour_location" class="regular-text" required /></td>
-                </tr>
-
-                <tr valign="top">
-                    <th scope="row"><label for="tour_duration">Duration:</label></th>
-                    <td><input type="text" name="tour_duration" id="tour_duration" class="regular-text" required /></td>
-                </tr>
-
-                <tr valign="top">
-                    <th scope="row"><label for="tour_price">Price:</label></th>
-                    <td><input type="number" name="tour_price" id="tour_price" class="regular-text" required /></td>
-                </tr>
-
-                <tr valign="top">
-                    <th scope="row"><label for="tour_availability">Availability:</label></th>
-                    <td><input type="text" name="tour_availability" id="tour_availability" class="regular-text" required /></td>
-                </tr>
-
-                <tr valign="top">
-                    <th scope="row"><label for="tour_cover_images">Cover Images (comma-separated URLs):</label></th>
-                    <td><input type="text" name="tour_cover_images" id="tour_cover_images" class="regular-text" required /></td>
-                </tr>
-
-                <tr valign="top">
-                    <th scope="row"><label for="rank_math_focus_keyword">Focus Keyword:</label></th>
-                    <td><input type="text" name="rank_math_focus_keyword" id="rank_math_focus_keyword" class="regular-text" /></td>
-                </tr>
-            </table>
-
-            <p class="submit">
-                <input type="submit" name="add_trip_submit" id="add_trip_submit" class="button-primary" value="Add Trip" />
-            </p>
-        </form>
-    </div>
-    <?php
-}
 ?>
