df5e2ec60be8f142e2085b9bf77093e1a9207159
PatraSuchi <101924210+Suchibrata-Patra@users.noreply.github.com>
2025-01-26 16:41:31 +0530

    Update TourDetailsEntry.php

--- Forms/TourDetailsEntry.php
+++ Forms/TourDetailsEntry.php
@@ -100,7 +100,7 @@ function display_tour_meta_box($post) {
                     </div>
                     <div class="form-group" style="margin-left:10px;">
                         <label for="tour_note">Note</label>
-                        <span name="tour_note">By Default, it will take <strong><i><u>Same Day tour Package.</u></i></strong> <br>For same day tour package choose Day = 1 and Nights = 0 </span>
+                        <span name="tour_note">By Default, it will take <strong><i><u>Same Day tour Package.</u></i></strong> <br>For same day tour package choose Day = 1 and Nights = 0.</span>
                     </div>
 
                 </div>
