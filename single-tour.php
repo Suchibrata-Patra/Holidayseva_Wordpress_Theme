41e703ae2110ab8ab9c525196fd5477f6d237927
PatraSuchi <101924210+Suchibrata-Patra@users.noreply.github.com>
2025-01-26 22:25:42 +0530

    changed

--- single-tour.php
+++ single-tour.php
@@ -203,7 +203,7 @@ get_header(); ?>
 <?php else : ?>
     <p>Google Map is not available for this tour.</p>
 <?php endif; ?>
-
+<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d15283654.009758947!2d72.10189489975913!3d20.736163659701365!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x30635ff06b92b791%3A0xd78c4fa1854213a6!2z4KSt4KS-4KSw4KSk!5e0!3m2!1shi!2sin!4v1737887574027!5m2!1shi!2sin" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
     </article>
     <?php
     endwhile;
