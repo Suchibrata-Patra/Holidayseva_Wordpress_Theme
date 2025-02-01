78db60d2fbb86553e6a658149ece6cc7ee36e1b5
PatraSuchi <101924210+Suchibrata-Patra@users.noreply.github.com>
2025-02-01 10:59:48 +0530

    Changed

--- single-tour.php
+++ single-tour.php
@@ -2,6 +2,9 @@
 <link rel="stylesheet" href="<?php echo get_template_directory_uri();?>/Assets/Central_styling.css">
 <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
 
+<link rel="stylesheet" href="Assets/Central_styling.css">
+<link rel="stylesheet" href="Assets/TourDetailsEntry.css">
+
 <div class="holidayseva_Holidays_Main_Wrapper">
     <div class="heading_container">
     <span class="main_package_name holidayseva_h2">
@@ -17,5 +20,25 @@
             
         </ul>
     </div>
+    </div>
+    <div class="grid-container">
+        <div class="grid-item featured-image">
+            <img src="https://cdn.pixabay.com/photo/2023/12/16/00/06/mountain-8451604_640.jpg" alt="Featured">
+        </div>
+        <div class="grid-item package-highlights">
+            <h3>Package Highlights</h3>
+            <ul>
+                <li>Gulmarg Gondola Cable Car Ride</li>
+                <li>Photoshoot in Kashmiri Attire</li>
+                <li>Day Excursion to Sonmarg</li>
+            </ul>
+        </div>
+        <div class="grid-item video">Video Placeholder</div>
+        <div class="grid-item activities">
+            <img src="activities.jpg" alt="Activities & Sightseeing">
+        </div>
+        <div class="grid-item property-photos">
+            <img src="property.jpg" alt="Property Photos">
+        </div>
     </div>
 </div>
