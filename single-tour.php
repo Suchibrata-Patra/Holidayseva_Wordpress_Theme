763b67495f0d518377148a56e7a43a4ff303988a
PatraSuchi <101924210+Suchibrata-Patra@users.noreply.github.com>
2025-02-01 08:56:44 +0530

    Changed

--- single-tour.php
+++ single-tour.php
@@ -1,7 +1,7 @@
 <link rel="stylesheet" href="<?php echo get_template_directory_uri();?>/Assets/TourDetailsEntry.css">
 <link rel="stylesheet" href="<?php echo get_template_directory_uri();?>/Assets/Central_styling.css">
 
-<div class="holidayseva_Theme_Main_Wrapper">
+<div class="holidayseva_Holidays_Main_Wrapper">
     <div class="heading_container">
     <span class="main_package_name holidayseva_h1">
         Most Wanted Kashmir Package
