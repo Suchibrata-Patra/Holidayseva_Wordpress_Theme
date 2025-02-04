1b43482b91039aa6834c04d6e9c5652295c4273d
PatraSuchi <101924210+Suchibrata-Patra@users.noreply.github.com>
2025-02-04 12:17:18 +0530

    Changed

--- single-tour.php
+++ single-tour.php
@@ -1,5 +1,6 @@
 <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/Assets/TourDetailsEntry.css">
 <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/Assets/Central_styling.css">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
 <link rel="stylesheet"
     href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
 <!-- <link rel="stylesheet" href="Assets/Central_styling.css"> -->
