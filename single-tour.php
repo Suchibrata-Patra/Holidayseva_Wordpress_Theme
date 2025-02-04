81a1dd1eeb4ac68761ff34ee880b24f1b6803d95
PatraSuchi <101924210+Suchibrata-Patra@users.noreply.github.com>
2025-02-04 11:44:59 +0530

    Changed

--- single-tour.php
+++ single-tour.php
@@ -1,21 +1,31 @@
+<title>Kashmir Tour</title>
 <link rel="stylesheet" href="<?php echo get_template_directory_uri();?>/Assets/TourDetailsEntry.css">
 <link rel="stylesheet" href="<?php echo get_template_directory_uri();?>/Assets/Central_styling.css">
-<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
+<link rel="stylesheet"
+    href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
 
 <link rel="stylesheet" href="Assets/Central_styling.css">
 <link rel="stylesheet" href="Assets/TourDetailsEntry.css">
 
-<div class="main_search_filter_container" style="background-color: #0a223d; margin:0px -8px 10px -8px !important;padding:3px;">
+<div class="main_search_filter_container"
+    style="background-color: #0a223d; margin:0px -8px 10px -8px !important;padding:3px;">
     <ul style="display: flex;list-style:none;gap:20px;margin-left:40px;">
-        <li style="font-size: 25px!important;"><button style="background:#233951 ;color:#0188f8;border: none;padding: 7px 20px;border-radius: 7px;">STATING FORM <br> <span style="color:white;">New Delhi</span></button></li>
-        <li style="font-size: 25px!important;"><button style="background:#233951 ;color:#0188f8;border: none;padding: 7px 20px;border-radius: 7px;">TRAVELLING ON<br> <span style="color:white;">Wed, 2 Apr 2025</span></button></li>
-        <li style="font-size: 25px!important;"><button style="background:#233951 ;color:#0188f8;border: none;padding: 7px 20px;border-radius: 7px;">ROOM & GUESTS<br> <span style="color:white;">2 Adults</span></button></li>
-        <li style="font-size: 25px!important;"><button style="background: linear-gradient(90deg, #50affd, #065bf2);color:#ffffff;border: none;padding: 10px 35px;border-radius: 50px;font-weight:400;font-size:20px;">Search</button></li>
+        <li style="font-size: 25px!important;"><button
+                style="background:#233951 ;color:#0188f8;border: none;padding: 7px 20px;border-radius: 7px;">STATING
+                FORM <br> <span style="color:white;">New Delhi</span></button></li>
+        <li style="font-size: 25px!important;"><button
+                style="background:#233951 ;color:#0188f8;border: none;padding: 7px 20px;border-radius: 7px;">TRAVELLING
+                ON<br> <span style="color:white;">Wed, 2 Apr 2025</span></button></li>
+        <li style="font-size: 25px!important;"><button
+                style="background:#233951 ;color:#0188f8;border: none;padding: 7px 20px;border-radius: 7px;">ROOM &
+                GUESTS<br> <span style="color:white;">2 Adults</span></button></li>
+        <li style="font-size: 25px!important;"><button
+                style="background: linear-gradient(90deg, #50affd, #065bf2);color:#ffffff;border: none;padding: 8px 55px;border-radius: 50px;font-weight:400;font-size:20px;">Search</button>
+        </li>
     </ul>
 </div>
 
-<div class="holidayseva_Holidays_Main_Wrapper">
-    <div class="heading_container">
+<div class="heading_container" style="position: sticky;margin-left:5%;z-index:2;background-color: white;padding: 3px;">
     <span class="main_package_name holidayseva_h2">
         Most Wanted Kashmir Packages
     </span>
@@ -25,10 +35,12 @@
             <li><button class="quickpanel_buttons">6N/7D</button></li>
             <li>1N Srinagar Houseboat</li>
             <li class="holidayseva_header_navbar_with_ul">2N Pahalgam</li>
-            <li class="holidayseva_header_navbar_with_ul">2N Srinagar</li> 
+            <li class="holidayseva_header_navbar_with_ul">2N Srinagar</li>
         </ul>
     </div>
-    </div>
+</div>
+
+<div class="holidayseva_Holidays_Main_Wrapper">
     <div class="grid-container">
         <div class="image-container">
             <img src="/Assets/Images/Local_Storage_only/House-boat.avif" alt="Featured">
@@ -39,9 +51,9 @@
                 </a>
             </div>
         </div>
-        
 
-                
+
+
 
         <!-- <div class="tour_page_grid_item package-highlights holidayseva_h6">
             <h3>Package Highlights</h3>
@@ -57,7 +69,8 @@
             <center><img src="https://cdn.pixabay.com/photo/2023/12/16/00/06/mountain-8451604_640.jpg" style="width:96%;height:35%;border-radius:2px;margin-top:15px;border-radius:5px;"></center>
         </div> -->
 
-        <div class="tour_page_grid_item package-highlights" style="border-radius: 5px;background-color: #fff0da;border: 1px solid #ffddaa;">
+        <div class="tour_page_grid_item package-highlights"
+            style="border-radius: 5px;background-color: #fff0da;border: 1px solid #ffddaa;">
             <button id="tour_package_highilights_button">PACKAGE HIGHLIGHTS</button>
             <div class="tour_page_grid_highlights_items">
                 <ul style="font-size:13px;">
@@ -65,22 +78,24 @@
                         <div style="display:flex;">
                             <span class="material-symbols-outlined" style="font-size:15px;line-height: 1rem;">
                                 check_circle
-                                </span> Gulmarg Gondola Cable Car..
+                            </span> Gulmarg Gondola Cable Car..
                         </div>
                         <span class="tour_highlights_display_day">Day 2 - Gulmarg</span>
                     </li>
-                    <li> <div style="display:flex;">
-                        <span class="material-symbols-outlined" style="font-size:15px;line-height: 1rem;">
-                            check_circle
+                    <li>
+                        <div style="display:flex;">
+                            <span class="material-symbols-outlined" style="font-size:15px;line-height: 1rem;">
+                                check_circle
                             </span> Gulmarg Gondola Cable Car ..
-                    </div>
+                        </div>
                         <span class="tour_highlights_display_day">Day 1 - Srinagar Houseboat</span>
                     </li>
-                    <li> <div style="display:flex;">
-                        <span class="material-symbols-outlined" style="font-size:15px;line-height: 1rem;">
-                            check_circle
+                    <li>
+                        <div style="display:flex;">
+                            <span class="material-symbols-outlined" style="font-size:15px;line-height: 1rem;">
+                                check_circle
                             </span> Gulmarg Gondola Cable Car ..
-                    </div>
+                        </div>
                         <span class="tour_highlights_display_day">Day 6 - Srinagar</span>
                     </li>
                 </ul>
@@ -88,7 +103,8 @@
             <div class="tour_page_grid_highlights_items">
                 <span class="holidayseva_h6" style="color:#008cff;font-weight: 500;">View All Highlights</span>
                 <div class="tour_page_video_highlights" style="margin-top: 10px;">
-                    <img src="https://hldak.mmtcdn.com/prod-s3-activity-templates/activitiesImagesFinal/activity/XGU/ACT10000000000371_3.jpeg">
+                    <img
+                        src="https://hldak.mmtcdn.com/prod-s3-activity-templates/activitiesImagesFinal/activity/XGU/ACT10000000000371_3.jpeg">
                 </div>
             </div>
         </div>
@@ -96,9 +112,10 @@
         <!-- <div class="tour_page_grid_item video">Video Placeholder</div> -->
         <div class="tour_page_grid_item activities">
             <div class="topight_top_video" style="padding-bottom:5px;">
-                <iframe width="225px" height="auto" src="https://www.youtube.com/embed/0K0cDY7miEc" frameborder="0" allowfullscreen></iframe>
+                <iframe width="225px" height="auto" src="https://www.youtube.com/embed/0K0cDY7miEc" frameborder="0"
+                    allowfullscreen></iframe>
             </div>
-            
+
             <div class="topight_bottom_video" style="padding-top:5px;">
                 <img src="https://cdn.pixabay.com/photo/2023/12/16/00/06/mountain-8451604_640.jpg" alt="">
                 <div class="overlay">Activities & Sightseeing</div>
@@ -108,10 +125,9 @@
         </div>
 
         <div class="tour_page_grid_item activities">
-            <img src="https://r1imghtlak.mmtcdn.com/c2a2b289-4950-4e04-947e-68416babcc67.jpg" alt="Activities & Sightseeing">
+            <img src="https://r1imghtlak.mmtcdn.com/c2a2b289-4950-4e04-947e-68416babcc67.jpg"
+                alt="Activities & Sightseeing">
             <div class="overlay">Property Photos</div>
         </div>
-        
     </div>
 </div>
-
