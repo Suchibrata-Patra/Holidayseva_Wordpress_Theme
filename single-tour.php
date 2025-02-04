96c0ba89c1c05539c16c3754ff9af215ee396509
PatraSuchi <101924210+Suchibrata-Patra@users.noreply.github.com>
2025-02-04 20:39:32 +0530

    Update single-tour.php

--- single-tour.php
+++ single-tour.php
@@ -26,7 +26,7 @@
 </div>
 
 <div class="holidayseva_Holidays_Main_Wrapper">
-    <div class="heading_container">
+    <div class="heading_container" style="position:sticky;">
         <span class="main_package_name holidayseva_h2">
             Most Wanted Kashmir Packages
         </span>
