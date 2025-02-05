8c64942523d4cab6992761f08102c5ee63412649
PatraSuchi <101924210+Suchibrata-Patra@users.noreply.github.com>
2025-02-05 23:57:44 +0530

    Update single-tour.php

--- single-tour.php
+++ single-tour.php
@@ -52,8 +52,8 @@
     </div>
     <div class="grid-container">
         <div class="image-container">
-            <img src="/Assets/Images/Local_Storage_only/House-boat.avif" alt="Featured">
-            <!-- <img src="<?php echo get_template_directory_uri(); ?>/Assets/Images/Local_Storage_only/House-boat.avif" alt="Featured"> -->
+            <!-- <img src="/Assets/Images/Local_Storage_only/House-boat.avif" alt="Featured"> -->
+            <img src="<?php echo get_template_directory_uri(); ?>/Assets/Images/Local_Storage_only/House-boat.avif" alt="Featured">
             <div class="image-overlay">
                 <a href="#" class="overlay-button">
                     <span class="icon"></span> VIEW GALLERY →
