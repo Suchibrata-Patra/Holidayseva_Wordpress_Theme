a41e30288a1e3e018ec3593798393cb7bd3392d0
PatraSuchi <101924210+Suchibrata-Patra@users.noreply.github.com>
2025-02-06 22:19:55 +0530

    Changed

--- single-tour.php
+++ single-tour.php
@@ -1,3 +1,4 @@
+<title>Tour</title>
 <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/Assets/TourDetailsEntry.css">
 <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/Assets/Central_styling.css">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -23,31 +24,34 @@
                 style="background: linear-gradient(90deg, #50affd, #065bf2);color:#ffffff;border: none;padding: 8px 55px;border-radius: 50px;font-weight:400;font-size:20px;">Search</button>
         </li>
     </ul>
-</div> 
+</div>
 
 <div class="holidayseva_Holidays_Main_Wrapper">
-    <div class="heading_container" style="z-index:2;position: sticky;display:flex;top: 0;background-color: white;padding: 3px;">
+    <div class="heading_container"
+        style="z-index:2;position: sticky;display:flex;top: 0;background-color: white;padding: 3px;">
         <div class="Header_TopLeftPortion">
             <span class="main_package_name holidayseva_h2">
                 Most Wanted Kashmir Packages
             </span>
             <div class="quick_details_navigation_bar">
                 <ul>
-                <div class="quick_details_navigation_bar">
-                <ul>
-                    <li><button class="quickpanel_buttons">Customizable</button></li>
-                    <li><button class="quickpanel_buttons" style="background-color: black;color: white;border: 1px solid white;">6N/7D</button></li>
-                    <li><strong style="color: black;font-weight: 400;">Seller:</strong> Holidayseva.com</li>
-                    <li class="holidayseva_header_navbar_with_ul">1N Srinagar Houseboat</li>
-                    <li class="holidayseva_header_navbar_with_ul">2N Pahalgam</li>
-                    <li class="holidayseva_header_navbar_with_ul">2N Srinagar</li>
-                </ul>
-            </div>
+                    <div class="quick_details_navigation_bar">
+                        <ul>
+                            <li><button class="quickpanel_buttons">Customizable</button></li>
+                            <li><button class="quickpanel_buttons"
+                                    style="background-color: black;color: white;border: 1px solid white;">6N/7D</button>
+                            </li>
+                            <!-- <li><strong style="color: black;font-weight: 400;">Seller:</strong> Holidayseva.com</li> -->
+                            <li>1N Srinagar Houseboat</li>
+                            <li class="holidayseva_header_navbar_with_ul">2N Pahalgam</li>
+                            <li class="holidayseva_header_navbar_with_ul">2N Srinagar</li>
+                        </ul>
+                    </div>
             </div>
         </div>
-        <div class="Header_TopRighttPortion" style="margin-left:15%;padding:2rem;" >
+        <div class="Header_TopRighttPortion" style="margin-left:15%;padding:2rem;">
             <span class="holidayseva_h3">Rs. 1999/-</span>
-            <button class="holidayseva_primary_button_size_1" style="margin-left: 2rem;"> Book Now</button>
+            <button class="holidayseva_primary_button button_size_2" style="margin-left: 2rem;"> Book Now</button>
         </div>
     </div>
     <div class="grid-container">
@@ -120,13 +124,79 @@
                 alt="Activities & Sightseeing">
             <div class="overlay">Property Photos</div>
         </div>
-       <div>
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Beatae impedit, voluptas, tenetur, repellat corrupti molestiae enim libero sit dignissimos quod et labore animi consequuntur quaerat iste eligendi accusantium possimus excepturi velit. Nisi, ratione? Hic, dolorum? Dolores tempora consequuntur nihil quasi ipsum! Nisi delectus ad nostrum laudantium sequi unde, quam voluptate itaque velit excepturi, consectetur voluptatibus ratione ex quia. Laborum voluptates similique unde maxime eveniet cupiditate, illum aliquid corporis quasi reprehenderit nesciunt nisi rerum vitae exercitationem, veritatis nemo ea excepturi accusamus soluta quaerat laudantium autem ipsum. Ratione quis, alias eaque nesciunt quaerat vel qui assumenda sunt cum veritatis deserunt pariatur laudantium, nisi praesentium quidem accusamus asperiores exercitationem officia facilis aliquam? Ipsam, tenetur rem ad fugiat repellendus eius dolores? Nesciunt iusto unde repudiandae, non doloribus dolores eaque, enim natus, facere at totam nihil! Veniam sint aperiam magnam consequatur, nihil cumque numquam eos eum, totam sunt laborum laudantium porro harum minima voluptatem ea aliquam architecto, quo quae necessitatibus beatae exercitationem. Impedit nulla optio repellendus exercitationem placeat suscipit recusandae praesentium. Officiis voluptatem magnam, 
-        <strong style="position: sticky !important;top:12.5vh;background-color: rgb(255, 0, 0);color: white;"> numquam quibusdam Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sapiente, autem deleniti. Hic at eos similique ab sed. Aut molestias rem similique officiis non illum aliquam, libero sequi architecto ex aliquid, fugit eveniet inventore in quisquam, qui tempora distinctio culpa labore? A labore dolorum obcaecati officia quos. Cupiditate, minus expedita!</strong>
-        alias illum nostrum voluptate! Quisquam aperiam optio iure itaque quibusdam, aliquid delectus recusandae quia, eveniet ipsa perspiciatis quis quam harum consectetur dolor 
-        neque maiores corporis impedit iure aliquam alias soluta deserunt vero. Necessitatibus accusantium sequi accusamus obcaecati adipisci? Neque hic dolor fugiat obcaecati odit commodi reiciendis esse, exercitationem sed at officia ratione repellat fugit nemo nostrum animi a voluptatem nam perspiciatis distinctio soluta. Nostrum culpa adipisci similique, ea vero, facilis in beatae, dolores nulla suscipit assumenda repudiandae reprehenderit eos consequatur voluptatibus fugiat? Magni aut dicta temporibus, modi delectus quia cum dignissimos nesciunt qui nulla deserunt similique nostrum ea rem repellat, voluptate ex dolore consequuntur vitae omnis suscipit autem ipsum. Provident quibusdam veritatis exercitationem eligendi dignissimos nostrum nam. Temporibus non quas asperiores laboriosam. Aut quaerat harum repellendus tenetur eaque non fuga laudantium asperiores molestiae nam quam maiores saepe quis veritatis optio aliquid vero, ratione culpa facilis delectus neque ad dolores. Hic praesentium, enim fugit laboriosam nostrum in voluptatibus labore, ratione ea voluptatum recusandae autem fugiat velit perspiciatis eos harum obcaecati reiciendis molestias quis. Consequatur fugit doloribus dicta consequuntur quidem nesciunt voluptatem deserunt magni natus. Repellat ducimus, assumenda quam quo saepe maxime quasi nam animi unde consequuntur numquam accusantium vel tempore, ullam facilis eaque vitae atque. Qui architecto facere vel aliquam fuga, aperiam illum quidem, debitis explicabo nesciunt dolorem totam iste? Rerum veritatis itaque voluptatem est corrupti perspiciatis asperiores, molestias qui. Quisquam cum pariatur inventore officiis voluptatibus nobis.
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptates mollitia, adipisci quos neque esse similique! Veniam, atque numquam? Quis consequatur quidem molestiae voluptate, dicta reprehenderit, velit vitae magnam sunt quam consequuntur commodi, facere eos nulla? Impedit unde pariatur perspiciatis quaerat magnam fugit veniam facere ad et sint ab, neque debitis doloribus nostrum id mollitia earum magni iste aliquam. Quos, possimus ipsam esse voluptas soluta eos quaerat asperiores, molestiae vel, aliquam fugiat molestias amet minus voluptatum. Nesciunt adipisci maiores perspiciatis eveniet animi aliquam doloribus sequi deserunt ex totam laborum, recusandae veritatis natus minus consectetur expedita non nihil eum, ullam voluptate! Sunt tenetur omnis earum! Vero, nisi! Exercitationem, tempore soluta dolores eligendi iure iusto a sunt iste aut in explicabo fugit sequi debitis, eveniet dolorem earum perferendis accusantium et molestias ipsa nam adipisci voluptatem voluptate itaque? Quos eos aliquam, debitis nisi recusandae labore suscipit eligendi veniam consectetur doloremque, in, facere id saepe tempore placeat ipsa voluptas quae accusamus rerum. Eligendi fugiat, nisi molestiae cumque provident itaque possimus at aut deleniti numquam illo in? Exercitationem odio sit iure, nobis officiis vel atque pariatur iste sint voluptas molestias rerum? Omnis atque fuga sed aliquid repudiandae. A provident atque harum rem quisquam delectus unde fuga nam dicta qui autem quo quasi dolores consequuntur, eveniet, repudiandae inventore molestias voluptate sit possimus officiis tenetur iure! Aperiam temporibus suscipit est corrupti! Soluta voluptatum fugiat consectetur dicta dolore reprehenderit atque sint voluptas facilis nulla repudiandae, aliquam aut, exercitationem nam eius magni aspernatur minima, unde dolores ipsa quisquam. Fugiat natus cumque aperiam quas magni corporis velit quod tempore non. Voluptas modi accusantium ullam ipsum sapiente nihil omnis quas nobis. Odio minima qui totam. Obcaecati dignissimos delectus laborum ipsum. Tenetur deleniti aut, temporibus hic accusamus dolorem dolore nesciunt doloremque nulla recusandae accusantium omnis sunt maiores eaque! Ratione illum possimus tempore sit deserunt nam, modi magnam dolore hic, quam tenetur dolorem doloremque inventore non ipsam quis, sed obcaecati. Odio quod asperiores ullam soluta, tempore odit qui laborum quibusdam saepe nisi dignissimos cumque consequatur ducimus a? Dignissimos facere dolorum consequuntur tenetur quaerat deserunt non! Soluta, quaerat quos cumque, magni magnam temporibus accusantium incidunt omnis, minus doloremque natus rerum facere earum! Ab ratione iure officiis velit sed veniam ipsa praesentium obcaecati recusandae! Voluptatem, laborum? Non delectus, amet quia similique sapiente vel quibusdam? Fuga quaerat, unde, beatae consectetur dolore quibusdam ullam velit nam reprehenderit eum molestias totam molestiae esse temporibus a sint accusamus? Aliquam, nostrum earum ullam recusandae ex ab perspiciatis quia pariatur tenetur fugiat beatae, eum cumque dolorem provident esse eligendi cupiditate porro? Neque rerum earum, sed laborum cumque quasi! Ipsum earum laudantium velit error esse alias quidem quisquam quasi similique sequi, harum possimus nemo voluptatibus maxime veritatis ratione maiores! Consectetur perspiciatis ex facilis voluptates ducimus velit sit, mollitia quidem sint vitae, voluptatibus provident eum repellendus reiciendis fugiat asperiores cumque architecto! Ullam ipsum aspernatur dignissimos laudantium necessitatibus, temporibus possimus ratione nam adipisci quos molestiae! Repellat corporis necessitatibus quae voluptatum nesciunt. Minima iusto, voluptatem tempora molestias maiores rerum? Animi tenetur ipsam maiores consequatur odio vel?
-       </div>
     </div>
-</div>
+    <!-- Starting Of the Description Details -->
+    <div style="width:100% !important;justify-content: center;">
+        <div class="tour_Details_navbar">
+            <ul style="display: flex;list-style: none;padding: 20px;padding-left:0px;font-weight: 500;font-size:1rem;color:rgb(95, 95, 95);">
+                <li style="padding: 10px;color: #008cff; padding:10px;border-bottom: 3px solid #008cff;">HIGHLIGHTS</li>
+                <li style="padding: 10px; padding:10px;"><a href="#itinerary" style="padding: 10px; padding:10px;color: rgb(95,95,95);text-decoration: none;">ITINERARY</a></li>
+                <li style="padding: 10px; padding:10px;"><a href="#hotels" style="padding: 10px; padding:10px;color: rgb(95,95,95);text-decoration: none;">HOTELS</a></li>
+                <li style="padding: 10px; padding:10px;"><a href="#policy" style="padding: 10px; padding:10px;color: rgb(95,95,95);text-decoration: none;">POLICY</a></li>
+                <li style="padding: 10px; padding:10px;"><a href="#reviews" style="padding: 10px; padding:10px;color: rgb(95,95,95);text-decoration: none;">REVIEWS</a></li>
+                <li style="padding: 10px; padding:10px;"><a href="#faq" style="padding: 10px; padding:10px;color: rgb(95,95,95);text-decoration: none;">FAQ</a></li>
+            </ul>
+        </div>
 
+        <div class="content">
+            <div id="itinerary">
+                <h2>Itinerary</h2>
+                <p>Here’s a detailed itinerary for your trip...</p>
+                <!-- Additional itinerary details can go here -->
+              </div>
+              
+              <div id="hotels">
+                <h2>Hotels</h2>
+                <p>Here are some hotel options for your stay...</p>
+                <ul>
+                  <li>Hotel 1: Description and price</li>
+                  <li>Hotel 2: Description and price</li>
+                  <li>Hotel 3: Description and price</li>
+                </ul>
+                <!-- More hotel information can be added here -->
+              </div>
+              
+              <div id="policy">
+                <h2>Policies</h2>
+                <p>Read through our terms and conditions, cancellation policy, and other important information...</p>
+                <ul>
+                  <li>Cancellation policy: Must cancel within 24 hours.</li>
+                  <li>Check-in time: 3:00 PM</li>
+                  <li>Check-out time: 11:00 AM</li>
+                  <!-- Other policy details can go here -->
+                </ul>
+              </div>
+              
+              <div id="reviews">
+                <h2>Reviews</h2>
+                <p>Read some reviews from past travelers...</p>
+                <div class="review">
+                  <h3>John Doe</h3>
+                  <p>"Had an amazing experience! Highly recommend!"</p>
+                </div>
+                <div class="review">
+                  <h3>Jane Smith</h3>
+                  <p>"Great service and accommodations. Would love to visit again!"</p>
+                </div>
+                <!-- Additional reviews can go here -->
+              </div>
+              
+              <div id="faq">
+                <h2>Frequently Asked Questions</h2>
+                <dl>
+                  <dt>What is included in the tour?</dt>
+                  <dd>The tour includes transportation, meals, and entry fees to attractions.</dd>
+                  
+                  <dt>Do I need travel insurance?</dt>
+                  <dd>It’s recommended, but not required, for all guests.</dd>
+                  
+                  <dt>Can I bring my pet?</dt>
+                  <dd>Pets are not allowed on the tour due to safety and comfort concerns.</dd>
+                </dl>
+                <!-- More FAQ questions and answers can go here -->
+              </div>
+              
+        </div>
+    </div> <!-- End of the Description Things -->
+</div>
+</div>
