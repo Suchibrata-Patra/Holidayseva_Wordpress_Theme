977eb5fcb6d9528f46b96895194096f45bc2a48e
PatraSuchi <101924210+Suchibrata-Patra@users.noreply.github.com>
2025-05-09 15:51:37 +0530

    Update index.php

--- index.php
+++ index.php
@@ -88,7 +88,7 @@ get_header(); ?>
     </div>
 
     <!-- Right: Text Centered -->
-    <div style="width: 50%; background-color:rgb(255, 255, 255); display: flex; align-items: center; justify-content: center;">
+    <div style="width: 50%; background-color:rgb(255, 255, 255); display: flex; align-items: center; justify-content: left;">
         <div style="font-size: 3rem; font-weight: bold; text-align: left;">
             We're Coming...
         </div>
