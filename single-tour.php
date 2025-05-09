efdb362fb2e5adf5632fc9a7ff7ef0c677ee4eb8
PatraSuchi <101924210+Suchibrata-Patra@users.noreply.github.com>
2025-05-09 15:13:03 +0530

    Changed

--- single-tour.php
+++ single-tour.php
@@ -1,3 +1,4 @@
+<title> Holidaysev </title>
 <!-- <?php require get_template_directory() . '/dropdownmenu.php'; ?> -->
 <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/Assets/TourDetailsEntry.css">
 <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/Assets/Central_styling.css">
