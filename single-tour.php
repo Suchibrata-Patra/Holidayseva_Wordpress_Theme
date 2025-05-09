1e675b20201d7f2cd2e105575958e81884b84ab0
PatraSuchi <101924210+Suchibrata-Patra@users.noreply.github.com>
2025-05-09 15:12:11 +0530

    Changed

--- single-tour.php
+++ single-tour.php
@@ -1,6 +1,7 @@
 <!-- <?php require get_template_directory() . '/dropdownmenu.php'; ?> -->
 <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/Assets/TourDetailsEntry.css">
 <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/Assets/Central_styling.css">
+<link rel="icon" type="image/x-icon" href="<?php echo get_template_directory_uri(); ?>/Assets/Images/favicon.svg">  
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <link rel="stylesheet"
     href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
