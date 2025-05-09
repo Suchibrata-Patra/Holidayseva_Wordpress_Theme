f5a6b00d97567f53b73226549cf92762b420c933
PatraSuchi <101924210+Suchibrata-Patra@users.noreply.github.com>
2025-05-09 15:11:17 +0530

    Changed

--- single-tour.php
+++ single-tour.php
@@ -1,4 +1,3 @@
-<?php get_header(); ?>
 <!-- <?php require get_template_directory() . '/dropdownmenu.php'; ?> -->
 <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/Assets/TourDetailsEntry.css">
 <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/Assets/Central_styling.css">
