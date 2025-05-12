38c7267452a7f1d636c1f726ddc1872eb8e38924
PatraSuchi <101924210+Suchibrata-Patra@users.noreply.github.com>
2025-05-12 08:59:35 +0530

    Changed

--- index.php
+++ index.php
@@ -7,6 +7,7 @@
 
 get_header(); ?>
 <main id="primary" class="site-main">
+<link rel="stylesheet" href="<?php echo $template_uri;?>/Assets/Central_styling.css">
 <?php
 // while (have_posts()) :
 //     the_post();
