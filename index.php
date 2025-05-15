7a36f143c9e929db31d1d662ff1dd201c9c1843f
PatraSuchi <101924210+Suchibrata-Patra@users.noreply.github.com>
2025-05-15 11:46:36 +0530

    Changed Version

--- index.php
+++ index.php
@@ -103,7 +103,7 @@ get_header(); ?>
         margin:0px;
       }
       .hotel-store-heading{
-        font-size:3px;
+        font-size:3px !important;
       }
       .hotel-card{
         width: 180px;
