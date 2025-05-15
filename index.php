7f1ba110f6c5b0ac6110c67d700f6a1f2dad81a4
PatraSuchi <101924210+Suchibrata-Patra@users.noreply.github.com>
2025-05-15 11:12:55 +0530

    Changed VErsion

--- index.php
+++ index.php
@@ -255,6 +255,7 @@ Model of the thigs to be develoed by the model of the things to be doe by the th
   }
 
   .offer-link {
+    position: absolute;
     color: #007bff;
     text-decoration: none;
     font-weight: 700;
