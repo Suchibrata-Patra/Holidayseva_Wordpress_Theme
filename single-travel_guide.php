4553340a459a4074e9bdd0d983da34dedb2ba25e
PatraSuchi <101924210+Suchibrata-Patra@users.noreply.github.com>
2025-06-07 09:00:52 +0530

    Update single-travel_guide.php

--- single-travel_guide.php
+++ single-travel_guide.php
@@ -1,1 +1,20 @@
-Hello 
+<?php get_header(); ?>
+
+<?php
+if ( have_posts() ) :
+    while ( have_posts() ) : the_post();
+        ?>
+        <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+            <?php
+            // This outputs the entire block content (your cover, columns, gallery, buttons, etc)
+            the_content();
+            ?>
+        </article>
+        <?php
+    endwhile;
+else :
+    echo '<p>No Travel Guide found.</p>';
+endif;
+?>
+
+<?php get_footer(); ?>
