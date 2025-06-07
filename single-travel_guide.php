61f8f006e447d6ea4b39ea01816583642fcb7db3
PatraSuchi <101924210+Suchibrata-Patra@users.noreply.github.com>
2025-06-07 08:43:37 +0530

    Create single-travel_guide.php

---
+++ single-travel_guide.php
@@ -0,0 +1,20 @@
+<?php
+if (have_rows('travel_guide_builder')) :
+    while (have_rows('travel_guide_builder')) : the_row();
+
+        if (get_row_layout() == 'hero_section') :
+            // Your markup
+            the_sub_field('heading');
+            echo wp_get_attachment_image(get_sub_field('image'), 'large');
+
+        elseif (get_row_layout() == 'itinerary_section') :
+            // Itinerary UI
+
+        elseif (get_row_layout() == 'gallery_section') :
+            // Gallery
+
+        endif;
+
+    endwhile;
+endif;
+?>
