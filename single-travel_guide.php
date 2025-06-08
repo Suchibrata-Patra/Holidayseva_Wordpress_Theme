e2c247b24c5551965a4e40739960f8eb3d94b28f
PatraSuchi <101924210+Suchibrata-Patra@users.noreply.github.com>
2025-06-08 22:08:33 +0530

    Update single-travel_guide.php

--- single-travel_guide.php
+++ single-travel_guide.php
@@ -35,7 +35,7 @@ if (have_posts()) :
         <h1 class="hero-title">Experience the Soul-Stirring<br> Treasures of Kakadu National Park</h1>
         <div class="author-box">
             Written By - 
-            <div class="author-name">Suchana Chakaborty</div>
+            <div class="author-name">HolidaySeva</div>
             <div class="author-date">Last updated: May 26, 2025</div>
         </div>
     </div>
