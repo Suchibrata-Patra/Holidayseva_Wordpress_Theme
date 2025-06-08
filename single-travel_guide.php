df32112ceb3e5db28be2405403e508c71d577417
PatraSuchi <101924210+Suchibrata-Patra@users.noreply.github.com>
2025-06-08 15:11:06 +0530

    Update single-travel_guide.php

--- single-travel_guide.php
+++ single-travel_guide.php
@@ -1,6 +1,6 @@
+ <link rel="stylesheet" href="<?php echo get_template_directory_uri();?>/Assets/Central_styling.css">
 <?php
 get_header();
-
 if (have_posts()) :
     while (have_posts()) : the_post();
 
