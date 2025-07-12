7709d12ff45aa689f860d3d9f9fc6d001198b4f0
PatraSuchi <101924210+Suchibrata-Patra@users.noreply.github.com>
2025-07-12 17:13:25 +0530

    Update single-travel_guide.php

--- single-travel_guide.php
+++ single-travel_guide.php
@@ -193,7 +193,7 @@ get_footer();
                             <?php echo $image_html; ?>
                         </div>
                         <div class="related_content_card_content">
-                            <span>Holidayseva Travel Guide</span>
+                            <span style="color:grey;font-size:0.5em;font-weight:400;">Holidayseva Travel Guide</span>
                             <p class="related_content_card_meta"><?php echo esc_html($categories_list); ?></p>
                             <!-- <h3 class="related_content_card_title"><?php the_title(); ?></h3> -->
                             <h3 class="related_content_card_title">Reinforcement learning for Modelling Marketplace Balance</h3>
