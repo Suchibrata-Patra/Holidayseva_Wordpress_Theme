85f56a040c66e68d76b5a4c55e9d2750c79abb85
PatraSuchi <101924210+Suchibrata-Patra@users.noreply.github.com>
2025-07-12 17:53:46 +0530

    Update single-travel_guide.php

--- single-travel_guide.php
+++ single-travel_guide.php
@@ -189,7 +189,7 @@ get_footer();
     }
 </style>
 
-<div style="display:flex;gap:20px;background-color:grey;padding:20px;">
+<div style="display:flex;gap:20px;background-color:rgb(229, 229, 229);padding:20px;">
     <div>This is the Sample Text</div>
     <div>
         <button style="border:none;padding:10px 20px;background-color:black;color:white;">FOllow on Linkedin</button>
