c6b9c6f9664db6da541c0fd0944e602acf3550ce
PatraSuchi <101924210+Suchibrata-Patra@users.noreply.github.com>
2025-07-14 17:09:33 +0530

    Changed

--- single-travel_guide.php
+++ single-travel_guide.php
@@ -189,10 +189,10 @@ if (have_posts()):
 
         <!-- ✅ Manually added 6th card styled like 'Most Popular' layout -->
 <div class="travel-guide-card" id="sixth_related_card" style="display: flex; justify-content: space-between; gap: 15px; margin-bottom: 20px;padding:30px 24px;background:none;">
-    
+
 <span style="font-size:1.3rem; color:black; font-weight:600; border-bottom:0.7px solid rgb(207, 207, 207); padding-bottom:6px; display:inline-block;">Most Popular</span>
 
-    <a href="/your-custom-link" style="text-decoration: none; display: flex; gap: 7px;">
+    <a href="/your-custom-link" style="text-decoration: none; display: flex !important;flex-direction:row; gap: 7px;">
         <div class="related_content_card_text" >
             <span style="color: rgb(107, 107, 107); font-size: 0.8rem; font-weight: 500;">
                 Engineering, Backend / 14 July / Global
